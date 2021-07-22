2ea5a8ebcc8ed6ff4be286ed036077c148bc8f6b
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2021-07-22 15:25:55 +0200

    card-step1

---
+++ script/script.js
@@ -0,0 +1,14 @@
+const card = {
+    nome: 'Bloodfire Colossus',
+    launchCost: ['6', 'R', 'R'],
+    conbinedManaCost: 8,
+    cardType: 'creatura',
+    subType: 'giant',
+    expasion: {
+        id: 9,
+        nome: 'Espansione ',
+        rarity: 'goldenrod'
+    }
+};
+
+console.log(card);
