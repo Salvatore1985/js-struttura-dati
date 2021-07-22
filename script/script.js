349b0a5a79fa875713555a86b9623e9d9d49572a
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2021-07-22 16:40:37 +0200

    subType-problema

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 const card = {
-    nome: 'Bloodfire Colossus',
+    nome: 'bloodfire colossus',
     launchCost: ['6', 'R', 'R'],
     conbinedManaCost: 8,
     cardType: 'creatura',
@@ -12,3 +12,29 @@ const card = {
 };
 
 console.log(card);
+
+/*STAMPO I PAGINA*/
+//recupero l'elemento dal DOM
+const cardSection = document.getElementById('cards');
+
+// const subType = card.subType ? card.subType : '';
+let subType;
+if (card.subType) {
+    subType = card.subType
+} else {
+    subType = '';
+}
+
+
+let cardTemplate = `
+<ul class" card text-capitalize ">
+<li ><strong>nome:</strong><em> ${card.nome}</em></li>
+<li><strong>costo di lancio:</strong> ${card.launchCost.join(', ')}</li>
+<li><strong>costo mana convertito:</strong> ${card.conbinedManaCost}</li>
+<li><strong>tipo di carta:</strong> ${card.cardType} ${subType}</li>
+
+
+</ul>
+`;
+
+cardSection.innerHTML = cardTemplate;
